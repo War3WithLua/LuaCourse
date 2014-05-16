0a671567734aa8beffdbf25a3aa01d0dab9da439
逸受 <1054044585@qq.com>
2014-05-16 19:42:06 +0800

    Update Lua作图教程.lua

--- Lua作图教程.lua
+++ Lua作图教程.lua
@@ -9,7 +9,7 @@
 print(...) --这个函数允许带几乎任何类型的参数，作用是将输入的内容打印在屏幕上，而打印的地点就是lua的解释器/CommandLine
 
 --lua里有这么几种变量类型: number string table boolean function 以及特殊的 nil 和 userdata
-
+--[[
 number 数字，无论是整数的1还是实数的1.01在lua里都是number，并没有jass那样的区分
 string 字符串，其他基本和jass中相同，注意区别是lua中字符串的符号种类比jass多，更自由
 table 表，允许存储任何类型的值，lua中大量的功能都是靠table实现的
@@ -17,15 +17,19 @@ boolean 布尔值，分true和false两种值
 function 函数，在lua中function是实实在在的一种变量类型，因此你可以将它赋值给一个变量
 nil 空，等同于jass中的null，在一个作用域结束后nil的值将会被清理掉
 userdata 自定义数据，引用小汐的话 "你可以将其理解为 jass 中的 handle,lua 无法直接对其进行修改"
+]]
 
 
 "lua的三种字符串符号:第一种:双引号，例如现在这个"  '第二种：由单引号限定范围'  [[第三种：两个方括号对应构成字符串的区域]]
 --"" 和 '' 不能跨行，如果需要跨行可以在字符串当行的最后面使用\延续 例如:
->print("这是测试字\
->>符")
-这是测试字
-符
---[[不过我更推荐]]"这是测试字\n符" --[[这种写法，其中的"\n"是一个转义符号，以后会再说明]]
+>print("This is\
+>>test string!")
+This is
+test string!
+--不过我更推荐:
+>print("This is\ntest string!") --[[这种写法，其中的"\n"是一个转义符号，以后会再说明]]
+This is
+test string!
 
 --lua的两种注释,第一种是直接用 符号" -- " ，从这个位置开始的本行剩余内容就是注释了
 --[==[第二种是 " --[[ things ]] " 两个负号和两对方括号，实际上就是注释的"--"加上了字符串的"[[]]"， ]==]
@@ -35,6 +39,7 @@ nesting of [[...]] is deprecated near '[' --在这里最后的方括号没有发
 >print("Test string"--[==[注释1[[注释2]]]==])
 Test string --注释没有意外的消失了
 
+--终于把注释提到了，那么就可以说明，本文中的小标题由[[]]内的框起，而注释和中文的说明则用--或--[[]]来限制
 
 [[lua中的保留词:]]
 and break do else elseif
@@ -44,3 +49,18 @@ repeat return then true until
 while
 
 [[lua的基本算数运算:]]
+>print(6+2)
+8
+>print(6-2)
+4
+>print(6*2)
+12
+>print(6/2)
+3
+>print(6^2) --幂运算
+36
+>print(6%2) --取模运算，用途甚广，使用便捷
+0
+>print(6+"1")
+7 --没错，在必要的时候lua会自动将string和number按照需求转换，但如果是不明确变量类型或其他特殊情况的时候最好使用tonumber()或tostring()进行转换
+>print()
