b365ac3ed8892d32457c87edc8db0ce174a4c630
逸受 <1054044585@qq.com>
2014-05-16 18:29:35 +0800

    Update Lua作图教程.lua

--- Lua作图教程.lua
+++ Lua作图教程.lua
@@ -1,5 +1,19 @@
 --[[
 既然是Lua作图教程，那么自然不能单单说lua或是作图，怎么结合运用就是这个教程的意义
-当然，大多数的教程都是这么说的]]
+当然，大多数的教程都是这么说的
+ 在 本 教 程 中 "lua作图" 都 简 称 为 "WL" 
+为什么是这么奇怪的名字可以自行脑补
+]]
 
-在最开始
+--作为lua,我觉得最先需要说明的是:
+print(...) --这个函数允许带几乎任何类型的参数，作用是将输入的内容打印在屏幕上，而打印的地点就是lua的解释器/CommandLine
+
+--[[lua里有这么几种变量类型: number string table boolean function 以及特殊的 nil 和 userdata
+number 数字，无论是整数的1还是实数的1.01在lua里都是number，并没有jass那样的区分
+string 字符串，其他基本和jass中相同，注意区别是lua中字符串的符号种类比jass多，更自由
+table 表，允许存储任何类型的值，lua中大量的功能都是靠table实现的
+boolean 布尔值，分true和false两种值
+function 函数，在lua中function是实实在在的一种变量类型，因此你可以将它赋值给一个变量
+nil 空，等同于jass中的null，在一个作用域结束后nil的值将会被清理掉
+userdata 自定义数据，引用小汐的话 "你可以将其理解为 jass 中的 handle,lua 无法直接对其进行修改"
+]]
